3dbf6aa3a0fb54db9c2e66a4bd271fabcbe8037f
Tusken Day Trader <167132663+TuskenDayTrader@users.noreply.github.com>
2025-08-08 01:39:43 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/LearnMorePage.tsx
+++ src/components/LearnMorePage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Card, CardContent, CardHeader, CardTit
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ArrowLeft, Heart, Users, Lightbulb, ChartBar } from '@phosphor-icons/react'
 import Logo from '@/components/Logo'
